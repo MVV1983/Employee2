4733f4c6a616829dbc9740a19e4cdd3073739045
Valentin <51223584+MVV1983@users.noreply.github.com>
2020-11-24 17:09:16 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ import java.util.*;
 
 public class Main {
 
+    static HashMap<Integer, Employee> listEmployee = new HashMap<>();
+    static List<String> nameList = new ArrayList<>();///просто список Имен всех сотрудников
+    static int personalNumber = 0;
+
+
     //Create Main menu
     public static void mainMenu() {
         System.out.println("Employee Application");
@@ -16,31 +21,29 @@ public class Main {
     public static int getMenuOptions() {
         int option;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your option: ");
+        System.out.print("Enter  PIN: ");
         option = sc.nextInt();
         return option;
     }
 
 
     public static void main(String[] args) {
-        Employee dev = new Developer(1, "Valentin", "IT", 5);
-        Employee dev2 = new Developer(2, "Irina", "Frontend", 4);
-        Employee booker = new Accountant(3, "Olga", "Bookers", 9);
+        //Employee dev = new Developer(1, "Valentin", "IT", 5);
+        //Employee dev2 = new Developer(2, "Irina", "Frontend", 4);
+        //Employee booker = new Accountant(3, "Olga", "Bookers", 9);
 
         //int grade = ((Developer)dev).getGrade();// Интерфейс не имеет метода getName, необходимо явное приведение
         // System.out.println(grade);
 
-        HashMap<Integer, Employee> listEmployee = new HashMap<>();
-        List<String> nameList = new ArrayList<>();///просто список Имен всех сотрудников
-        listEmployee.put(1, dev);
-        listEmployee.put(2, dev2);
-        listEmployee.put(3, booker);
+        //HashMap<Integer, Employee> listEmployee = new HashMap<>();
+        //List<String> nameList = new ArrayList<>();///просто список Имен всех сотрудников
+        //listEmployee.put(1, dev);
+        //listEmployee.put(2, dev2);
+        //listEmployee.put(3, booker);
 
-
-        for (Employee value : listEmployee.values()) {
-            nameList.add(value.getName());
-        }
-
+        //for (Employee value : listEmployee.values()) {
+        //    nameList.add(value.getName());
+        //}
 
         int op = 0;
 
@@ -49,16 +52,30 @@ public class Main {
             mainMenu();
             //Get menu options
             op = getMenuOptions();
+            int personalId = 0;
 
             switch (op) {
                 case 1://Add
-                    System.out.println("Add");
+                    System.out.println("********************");
+
+                    String nameEmployee = getParam("Enter name Employee: ");
+                    String job = getParam("Enter name job: ");
+                    personalId++;
+
+                    createEmployee(personalId, nameEmployee, job);
+
+                    //for (Employee value : listEmployee.values()) {
+                    //   nameList.add(value.getName());
+                    //}
+
+                    System.out.println("********************");
                     break;
                 case 2://Print All
                     System.out.println("Print all employee");
+                    System.out.println("********************");
                     //покажем имена всех сотрудников
-                    printAllEmployee(nameList);
-                    System.out.println("*****");
+                    printAllEmployee();//printAllEmployee(nameList);
+                    System.out.println("********************");
                     break;
                 case 3://Print salary
                     System.out.println("Print salary");
@@ -79,10 +96,67 @@ public class Main {
 
     }
 
-    public static void printAllEmployee(List<String> nameList) {
+    public static void printAllEmployee() { //(List<String> nameList)
 
-        for (String u : nameList) {
-            System.out.println(u);
+        if (listEmployee.size() != 0) {
+            for (Employee unit : listEmployee.values())
+                unit.print();
+        } else {
+            System.out.println("Лист сотрудников пуст");
         }
+    }
+
+    public static void createEmployee(int id, String name, String job) {
+        System.out.println("Выберите подразделение сотрудника " + "\n" + " 1: Developer;2: IT;3: Job;4: Exit");
+        int jobClass = 0;
+        int employeeClass = 0;
+        do {
+            jobClass = getMenuOptions();
+            switch (jobClass) {
+                case 1:
+
+                    Employee dev = new Developer(id, name, job, 5);
+                    //dev.print();
+                    createMapEmployee(dev);
+                    System.out.println("4: Exit");
+                    break;
+                case 2:
+
+                    System.out.println("4: Exit");
+                    break;
+                case 3:
+                    Employee booker = new Accountant(id, name, job, 9);
+                    createMapEmployee(booker);
+                    System.out.println("4: Exits");
+                    break;
+                case 4://Exit
+                    System.out.println("System exit");
+                    break;
+                default:
+                    System.out.println("INPUT ERROR");
+            }
+        }
+
+        while (jobClass != 4);
+
+    }
+
+    public static void createMapEmployee(Employee employee) {
+
+        listEmployee.put(personalNumber, employee);
+        personalNumber++;
+        //for (Employee value : listEmployee.values()) {
+        //    System.out.println(value.getName()+" "+value.getGrade());
+        //}
+    }
+
+    public static String getParam(String input) {
+
+        String option;
+        Scanner sc = new Scanner(System.in);
+        System.out.print(input);
+        option = sc.nextLine();
+
+        return option;
     }
 }
